8b748a36f9a0f14bdec64bc7215e9fd8d1754c8d
unimat <unimat@unimat.hu>
2022-11-04 15:19:02 +0100

    Extracting Directory Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import CategoryItem from './components/category-item/category-item.component';
+import Directory from './components/directory/directory.component';
 
 import './categories.styles.scss'
 
@@ -30,13 +30,9 @@ const App = () => {
       imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     },
   ];
-  return (
-      <div className='categories-container'>
-        {categories.map((category) => (
-          <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+
+  return <Directory categories={categories} />;
+
 };
 
 export default App;
