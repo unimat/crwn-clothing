0386ccf39ee2c2d44c58828f2ac5701879c12ad2
unimat <unimat@unimat.hu>
2022-11-05 10:58:16 +0100

    Implementing the nested page structure

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 
+const Shop = () => {
+  return <h1>I am a shop page</h1>;
+};
+
+const Other = () => {
+  return <h1>I am the other page</h1>;
+};
+
 const App = () => {
   return (
     <Routes>
-      <Route path='/' index element={<Home />} />
+      <Route path='/home' element={<Home />}>
+        <Route path='shop' element={<Shop />} />
+        {/* /home/shop */}
+        <Route path='other' element={<Other />} />
+        {/* /home/other */}
+      </Route>
     </Routes>
   );
 };
