4bbdf58be7911094c09a9c8af6ca0f8b3313f4db
unimat <unimat@unimat.hu>
2022-11-05 11:24:53 +0100

    Make thhe Navigation Bar persistent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the Navigation Bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+};
 
 const Shop = () => {
   return <h1>I am a shop page</h1>;
 };
 
-const Other = () => {
-  return <h1>I am the other page</h1>;
-};
-
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home />}>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
-        {/* /home/shop */}
-        <Route path='other' element={<Other />} />
-        {/* /home/other */}
       </Route>
     </Routes>
   );
