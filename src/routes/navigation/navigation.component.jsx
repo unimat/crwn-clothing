e31e2103ccb930b363ca6c4cf29a37560697f0a0
unimat <unimat@unimat.hu>
2022-11-08 14:18:10 +0100

    Implementing Shop, ProductContext ShoppingCartIcon component and its context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,7 +2,10 @@ import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils'
 
@@ -10,6 +13,7 @@ import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser} = useContext(UserContext)
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -23,14 +27,18 @@ const Navigation = () => {
           </Link>
           {
             currentUser ? (
-              <span className='nav-link' onClick={ signOutUser }>SIGN OUT</span>
+              <span className='nav-link' onClick={ signOutUser }>
+                SIGN OUT
+              </span>
               ) : 
               (<Link className='nav-link' to='/auth'>
                   SIGN IN
                 </Link>
               )
           }
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
